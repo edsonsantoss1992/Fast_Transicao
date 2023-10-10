b9e88535302ace7a9a0f183b36700674d2710527
edsonsantoss1992 <147215098+edsonsantoss1992@users.noreply.github.com>
2023-10-10 14:53:42 -0300

    Update Desafio02.py

--- Desafio02_Python/Desafio02.py
+++ Desafio02_Python/Desafio02.py
@@ -3,11 +3,14 @@ http://dados.recife.pe.gov.br/dataset/perfil-das-pessoas-vacinadas-covid-19/reso
 
 Usando a biblioteca pandas, faça alguns filtros  no dataset do link anterior
 
-Selecione as pessoas vacinadas de Recife do sexo feminino da cor parda e preta maior de 60 anos
-Selecione as pessoas  que se vacinaram nos meses de abril e maio do sexo masculino
+1-Selecione as pessoas vacinadas de Recife do sexo feminino da cor parda e preta maior de 60 anos
+2-Selecione as pessoas  que se vacinaram nos meses de abril e maio do sexo masculino
 Além do código, escreva no reade.me como você chegou na solução.'''
 
 import pandas as pd
-
+#1Questao
 df_arquivo= pd.read_csv("C:/Users/PC/Downloads/vacinados.csv",sep=",")
 df_arquivo[(df_arquivo['sexo']=='FEMININO') & ((df_arquivo['raca_cor']=='PARDA') | (df_arquivo['raca_cor']=='PRETA')) & (df_arquivo['idade']>60) ]
+#2Questao
+df_arquivo['data_vacinacao']= pd.to_datetime(df_arquivo['data_vacinacao'])
+segunda_consulta= df_arquivo[(df_arquivo['data_vacinacao'].dt.month.isin([11,12])) & (df_arquivo['sexo']=='MASCULINO')]
