492ecff555d6cbcc733433262039793a74fe6c52
edsonsantoss1992 <147215098+edsonsantoss1992@users.noreply.github.com>
2023-10-09 22:02:33 -0300

    Create Desafio02.py

---
+++ Desafio02_Python/Desafio02.py
@@ -0,0 +1,13 @@
+'''Para fazer o desafio, use o seguinte csv:
+http://dados.recife.pe.gov.br/dataset/perfil-das-pessoas-vacinadas-covid-19/resource/9664de94-9f07-4adc-848d-b6ef56510762 
+
+Usando a biblioteca pandas, faça alguns filtros  no dataset do link anterior
+
+Selecione as pessoas vacinadas de Recife do sexo feminino da cor parda e preta maior de 60 anos
+Selecione as pessoas  que se vacinaram nos meses de abril e maio do sexo masculino
+Além do código, escreva no reade.me como você chegou na solução.'''
+
+import pandas as pd
+
+df_arquivo= pd.read_csv("C:/Users/PC/Downloads/vacinados.csv",sep=",")
+df_arquivo[(df_arquivo['sexo']=='FEMININO') & ((df_arquivo['raca_cor']=='PARDA') | (df_arquivo['raca_cor']=='PRETA')) & (df_arquivo['idade']>60) ]
